77d98be3c79b0f420fd259c29bcf74d499228b21
Rebeca Brandão <58791853+rebecabrandao1@users.noreply.github.com>
2024-04-22 19:06:58 -0300

    Update main.cpp

--- icoleta/src/main.cpp
+++ icoleta/src/main.cpp
@@ -5,10 +5,15 @@
 const char* ssid = "EASY_LAB";
 const char* password = "_easy_lab_";
 
+// Pinos do sensor HX711
 const int LOADCELL_DOUT_PIN = 2;
 const int LOADCELL_SCK_PIN = 3;
 
 HX711 scale;
+
+
+const char* token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ3ZWlnaHQiOjkuMDgsInBvaW50X2lkIjoxLCJpYXQiOjE3MTA0OTc5Mjh9.EkeireB8pAYu7UYzCcZo11EaNt6rrlZlmfbxaXNTIZM";
+String baseUrl = "https://icoleta.netlify.app/descarte/";
 
 void setup() {
   Serial.begin(115200);
@@ -28,9 +33,9 @@ void loop() {
   if (scale.is_ready()) {
     long reading = scale.read();
   
-    if(WiFi.status() == WL_CONNECTED) {
+    if (WiFi.status() == WL_CONNECTED) {
       HTTPClient http;
-      http.begin("http://blabla/receber_peso"); 
+      http.begin(baseUrl + token); 
       http.addHeader("Content-Type", "application/x-www-form-urlencoded");
 
       String httpRequestData = "peso=" + String(reading); 
@@ -40,14 +45,12 @@ void loop() {
         String response = http.getString();
         Serial.println(httpResponseCode);
         Serial.println(response);
-      }
-      else {
+      } else {
         Serial.print("Erro no envio: ");
         Serial.println(httpResponseCode);
       }
       http.end();
-    }
-    else {
+    } else {
       Serial.println("Erro na conexão WiFi");
     }
   } else {
